57db9c7ea46d086449d796033b0d29be7deb73be
serii <seriiburduja@gmail.com>
2020-02-20 18:54:52 +0200

    header

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,22 +1,14 @@
 $(function () {
-  /*
-      https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
-      ===========================*/
-  $("html").easeScroll({
-    frameRate: 60,
-    animationTime: 1000,
-    stepSize: 90,
-    pulseAlgorithm: 1,
-    pulseScale: 8,
-    pulseNormalize: 1,
-    accelerationDelta: 20,
-    accelerationMax: 1,
-    keyboardSupport: true,
-    arrowScroll: 50,
-    touchpadSupport: true,
-    fixedBackground: true
-  });
+	$('.main-menu-mobile__img').on('click', function () {
+		$(this).parent('.main-menu-mobile__item').find('.main-menu-mobile-submenu').slideToggle();
+	});
 
+	$('#js-sandwitch-wrap').on('click', function () {
+		$('#js-main-menu-mobile').slideToggle();
+
+		$(this).toggleClass('sandwitch--active');
+		$(this).closest('.sandwitch-wrapper').toggleClass('sandwitch--active');
+	});
 });
 
 
