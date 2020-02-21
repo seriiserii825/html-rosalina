e9cf5760e64183bac0a28f067ef645aabce646a9
serii <seriiburduja@gmail.com>
2020-02-21 11:32:14 +0200

    eventi news block

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -15,7 +15,15 @@ $(function () {
 		$(this).toggleClass('sandwitch--active');
 		$(this).closest('.sandwitch-wrapper').toggleClass('sandwitch--active');
 	});
+
+	$('#js-eventi-news-block__content').append('<iframe id="iframe" src="https://web4.deskline.net/rosolina/it/event/list" ;="" scrolling="no" width="100%" height="2140" style=" overflow-x:scroll;"></iframe>');
+
 });
+
+// $(window).on('load', function () {
+// 	$('#js-eventi-news-block__content').appendChild('<iframe id="iframe" src="https://web4.deskline.net/rosolina/it/event/list" ;="" scrolling="no" width="100%" height="2140" style=" overflow-x:scroll;"></iframe>');
+// });
+
 
 
 
