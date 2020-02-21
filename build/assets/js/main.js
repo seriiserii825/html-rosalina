5f00d1c33dbe2907008afcf0b9089a804c935486
serii <seriiburduja@gmail.com>
2020-02-21 13:55:51 +0200

    build

---
+++ build/assets/js/main.js
@@ -0,0 +1,71 @@
+$(function () {
+	$('.main-menu-mobile__img').on('click', function () {
+		$(this).parent('.main-menu-mobile__item').find('.main-menu-mobile-submenu').slideToggle();
+	});
+
+	$('.main-menu__item').hover(function () {
+		$(this).find('.sub-menu').fadeIn();
+	}, function () {
+		$(this).find('.sub-menu').fadeOut();
+	});
+
+	$('#js-sandwitch-wrap').on('click', function () {
+		$('#js-main-menu-mobile').slideToggle();
+
+		$(this).toggleClass('sandwitch--active');
+		$(this).closest('.sandwitch-wrapper').toggleClass('sandwitch--active');
+	});
+
+	$('#js-eventi-news-block__content').append('<iframe id="iframe" src="https://web4.deskline.net/rosolina/it/event/list" ;="" scrolling="no" width="100%" height="2140" style=" overflow-x:scroll;"></iframe>');
+
+
+	let mail = function () {
+
+		if ($('#js-form').length > 0) {
+
+			$('#js-form').validate({
+				rules: {
+					email: {
+						required: true,
+						email: true
+					},
+					name: {
+						required: true,
+					},
+					oggetto: {
+						required: true,
+					},
+					messaggio: {
+						required: true,
+					},
+					check: {
+						required: true
+					}
+				},
+				messages: {
+					email: {
+						required: "Il campo è obbligatorio",
+						email: "Inserisci il campo email"
+					},
+					name: {
+						required: "Il campo è obbligatorio",
+					},
+					oggetto: {
+						required: "Il campo è obbligatorio",
+					},
+					messaggio: {
+						required: "Il campo è obbligatorio",
+					},
+					check: {
+						required: "Il campo è obbligatorio"
+					}
+				},
+			});
+		}
+	};
+	mail();
+
+});
+
+
+
