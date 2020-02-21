01e9c2d0a830a55c63f37a8d217552f679a20a47
serii <seriiburduja@gmail.com>
2020-02-21 09:27:48 +0200

    header mobile

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,6 +3,12 @@ $(function () {
 		$(this).parent('.main-menu-mobile__item').find('.main-menu-mobile-submenu').slideToggle();
 	});
 
+	$('.main-menu__item').hover(function () {
+		$(this).find('.sub-menu').fadeIn();
+	}, function () {
+		$(this).find('.sub-menu').fadeOut();
+	});
+
 	$('#js-sandwitch-wrap').on('click', function () {
 		$('#js-main-menu-mobile').slideToggle();
 
